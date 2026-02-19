a0c4f2d8f0af002b9905c2972b507e70f9e1e8a0
parthenobae <mdsafiullahanas@gmail.com>
2026-02-19 19:54:08 +0530

    Basic architecture's been built

---
+++ neurix/main/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, Blueprint
+from neurix.models import Post
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@main.route("/blogs")
+def blogs():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('blogs.html', title='Blogs', posts=posts)
