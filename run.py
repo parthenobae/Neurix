185965fc5feda249081fc6747644038bd0984c9d
Safi <mdsafiullahanas@gmail.com>
2026-02-27 15:07:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from neurix import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, debug=True)
